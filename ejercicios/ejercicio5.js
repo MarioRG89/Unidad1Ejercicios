06f8e1b3a24c931fb37d5c4cfcae800a18b94fb2
mario <mariorebollogarcia89@gmail.com>
2020-09-19 12:33:04 +0200

    ejercicio5

---
+++ ejercicios/ejercicio5.js
@@ -0,0 +1,16 @@
+function pedirFecha(){
+    var fecha= prompt("Introduzca la fecha de la siguiente manera: Hoy es seguido del día, el mes y el año de la siguiente forma")
+ }
+ function combinacionNumeros(){
+   var1= parseInt(prompt("introduzca un numero"));
+   var2= parseInt(prompt("introduzca otro numero"));      
+   alert(var1 * var2);
+   alert(var1 / var2);
+   alert(var1 - var2);
+   alert(var1^2 - var2^2);
+   if(var1>0){
+       alert(var1);
+   }else if(var2>0){
+       alert(var2);s
+   }
+ }
