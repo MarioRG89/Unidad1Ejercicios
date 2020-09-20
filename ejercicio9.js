9c586deb0365d4f3a49eca0b26c66056c5d094fd
mario <mariorebollogarcia89@gmail.com>
2020-09-20 10:35:05 +0200

    ejercicio9

---
+++ ejercicio9.js
@@ -0,0 +1,16 @@
+function numImpar(){
+    var i=0;
+    while(i<=35){
+        if(i%2!=0){
+            console.log(i)
+        }
+      i++;  
+    }
+}
+function numImpar2(){
+    for (var i = 0; i < 35; i++) {
+        if(i%2!=0){
+            console.log(i)
+        }
+      }
+}
